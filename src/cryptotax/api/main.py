9eff51dd163f46efb45c984a752b046eea05c546
perinst <cinepa.org@gmail.com>
2026-02-24 20:32:15 +0700

    stage(cors): update CORS settings to allow all origins

--- src/cryptotax/api/main.py
+++ src/cryptotax/api/main.py
@@ -44,7 +44,7 @@ async def global_exception_handler(request: Request, exc: Exception):
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
